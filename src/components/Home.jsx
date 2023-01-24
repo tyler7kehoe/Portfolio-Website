f02f2a90c65e7fb70e73196d8c59f1ad79ee62c5
tyler7kehoe <ttkehoe@gmail.com>
2023-01-24 16:18:01 -0600

    Home changes to include info

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,8 @@ export const Home = () => {
   return (
     <div className="home_mainBox">
         <div class="name-header">
-          <h1>My name is <span>Tyler Kehoe</span>.</h1>
+          <h1>My name is <span id='name'>Tyler Kehoe</span>.</h1>
+          <h2>I am a Computer Science student and an aspiring Software Engineer</h2>
         </div>
         <div className='headshot-image'>
           <img src={headshot} alt='Tyler Kehoe Headshot' />

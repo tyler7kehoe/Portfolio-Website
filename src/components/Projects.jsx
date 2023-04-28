a777db6ad21a68dee46aa5769cc5093a9ad3a3d6
tyler7kehoe <ttkehoe@gmail.com>
2023-04-28 11:03:56 -0500

    card update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,10 +5,11 @@ import ProjectCard from './ProjectCard'
 export const Projects = () => {
   return (
     <div className='projects-list'>
+      <h1>Check out my projects!</h1>
       <ProjectCard 
         img='https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0a6a49cf127bf92de1e2_icon_clyde_blurple_RGB.png'
         title='Argus Discord Bot'
-        desc='Argus is a project I completed during my Junior year of college. I worked for a friend that was a moderator in the Tasty Bones Discord Server. Devloped used Python and Discord.py.'
+        desc='Argus is a project I completed during my Junior year of college. I worked for a friend that was a moderator in the Tasty Bones Discord Server. Devloped used Python, Discord API and Discord.py.'
         link='https://github.com/tyler7kehoe/Argus'
       />
       <ProjectCard

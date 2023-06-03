1dfbc0ed18ce609952bb61986068efc10192870e
tyler7kehoe <ttkehoe@gmail.com>
2023-06-03 12:58:23 -0500

    small contact tweak

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,6 +6,7 @@ export const Contact = () => {
   return (
     <div className='contact-me'>
       <h1>Feel free to reach out to me at: ttkehoe@gmail.com</h1>
+      <h2>I dont bite!</h2>
     </div>
   )
 }

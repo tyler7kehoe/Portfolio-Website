339a5ee3d72642d05276f6cbf8f0e9bd16513e2f
tyler7kehoe <ttkehoe@gmail.com>
2023-06-03 12:59:21 -0500

    fix test commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,6 @@ export const Contact = () => {
   return (
     <div className='contact-me'>
       <h1>Feel free to reach out to me at: ttkehoe@gmail.com</h1>
-      <h2>I dont bite!</h2>
     </div>
   )
 }

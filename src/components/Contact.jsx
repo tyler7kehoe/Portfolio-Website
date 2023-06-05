a3b97dca657b8fd7cfcdd854815078b0b5d43074
tyler7kehoe <ttkehoe@gmail.com>
2023-06-04 20:46:06 -0500

    Made entire site mobile responsive

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ import '../css/Contact.css'
 export const Contact = () => {
   return (
     <div className='contact-me'>
-      <h1>Feel free to reach out to me at: ttkehoe@gmail.com</h1>
+      <h1>Feel free to reach out to me at: <span id='email'>ttkehoe@gmail.com</span></h1>
     </div>
   )
 }
